bfe90d5df37cd0ba9542a22d60b988d8b9ebd42c
luluconcha <iris.mihu@gmail.com>
2023-11-08 09:10:21 +0100

    fetching gov data, scraping addresses from senate

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,33 +1,51 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [error, setError] = useState("")
+  const [loading, setLoading] = useState(false)
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    getDatasetFromGobByTheme()
+    setLoading(true);
+    setError("")
+  }
+
+  async function getDatasetFromGobByTheme() {
+    try {
+      const response = await fetch(`https://datos.gob.es/apidata/catalog/dataset/theme/medio-ambiente?_sort=title&_pageSize=10&_page=0`)
+      if (!response.ok) throw new Error(`Oops! ${response.status} ${response.statusText}`)
+      const dataset = await response.json()
+      console.log(dataset)
+    }
+    catch(e) {
+      setError(e.message)
+    }
+    finally {
+      setLoading(false);
+    };
+  }
+
+ async function getSenadors() {
+    try {
+      const res = await fetch("/api/senadors");
+      if (!res.ok) throw new Error(`Oops! ${res.status} ${res.statusText}`);
+      const listSens = await res.json();
+      console.log("in app.jsx res was jsoned : " + listSens)
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      setLoading(false);
+    }
+  }
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     Welcome to the Green Army
+     <button onClick={(e) => handleSubmit(e)}> u click me i give dataset </button>
+     <button onClick = {getSenadors}> oi oi oi the people</button>
     </>
   )
 }
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,82 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const db = require("../model/helper");
+const puppeteer = require('puppeteer')
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send('index', { title: 'Express' });
+res.send('index', { title: 'Express' });
 });
+
+
+
+async function getEmailAddressesOfSenators() {
+  //// open a new browser, args are setting the visibility and view
+  const browser = await puppeteer.launch({ headless:false, defaultViewport: null});
+  const page = await browser.newPage();
+  await page.goto("https://www.senado.es/web/relacionesciudadanos/participacion/senadores/index.html", {waitUntil: "domcontentloaded"})
+  
+  const addresses = await page.evaluate(() => {
+    const allSenators = document.querySelectorAll("li.alterna three-col, li.three-col")
+    
+    const arrayOfsens = []
+    for (let sen of allSenators) {
+      let email = sen.querySelector("span.col-3 > a")
+      let nameSen = sen.querySelector("span.col-1 > a")
+      let affiliationSen = sen.querySelector("span.col-2 > acronym")
+
+      if (email.href.startsWith("mailto:")) {
+        const senator = {
+          name : nameSen.innerText,
+          email: email.href.substring(7),
+          affiliation : affiliationSen.title
+        }
+        arrayOfsens.push(senator)
+      }
+
+    }
+    console.log(arrayOfsens)
+  })
+
+  await new Promise(r => setTimeout(r, 240000));
+  await browser.close()
+} 
+
+getEmailAddressesOfSenators();
+
+// async function getEmailAddressesOfDiputados() {
+//   const browser = await puppeteer.launch({ headless:false, defaultViewport: null});
+//   const page = await browser.newPage();
+//   let numberOfDiputados = 350;
+//   for (let i = 0; i < numberOfDiputados; i++) {
+//     await page.goto("https://www.congreso.es/es/busqueda-de-diputados?p_p_id=diputadomodule&p_p_lifecycle=0&p_p_state=normal&p_p_mode=view&_diputadomodule_mostrarFicha=true&codParlamentario="+i+"&idLegislatura=XV&mostrarAgenda=false", {waitUntil: "domcontentloaded"})
+//     let emails = await page.evaluate(() => {
+//       const email = document.querySelector("div.email-dip > a")
+//       console.log(email)
+//     })
+//   }
+
+  // await page.goto("https://www.congreso.es/es/busqueda-de-diputados", {waitUntil: "domcontentloaded"})
+
+  // const addresses = await page.evaluate(async () => {
+  //   const bodyOfPage = document.querySelector("#_diputadomodule_contentPaginationDiputados > table > tbody")
+  //   for (let dip in bodyOfPage) {
+  //     const nextLink = document.querySelector(`${dip} > tr > th > a`)
+  //     await page.goto(nextLink.href)
+  //     const getMailDip = await page.evaluate(() => {
+  //       const email = document.querySelector("div.email-dip > a").href
+  //       console.log(email)
+  //     })
+  //   }
+  // })
+
+  // await new Promise(r => setTimeout(r, 240000));
+  // await browser.close()
+
+// }
+
+// getEmailAddressesOfDiputados();
+
 
 module.exports = router;
